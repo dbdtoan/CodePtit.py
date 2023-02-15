090fc5b39e9296a187bf4753695c8e7394bea6b3
dbdtoan <79196686+dbdtoan@users.noreply.github.com>
2023-02-15 15:48:42 +0700

    Update icpc0110.py

--- icpc0110.py
+++ icpc0110.py
@@ -1,1 +1,20 @@
-print("Xin chào thế giới!)
+import sqlite3
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+
+query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"
+
+c.execute(query)
+
+result = c.fetchone()
+
+if result:
+    print("Login successful")
+else:
+    print("Login failed")
+    
+conn.close()
